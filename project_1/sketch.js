8255efddd25a51e95511140f6e32c936927a485c
ericksandoval1 <ericksandoval1414@gmail.com>
2026-06-22 15:30:31 -0700

    project 1 change

--- project_1/sketch.js
+++ project_1/sketch.js
@@ -1,233 +1,247 @@
-let recMode = false;
-let can;
-let circdot1, circdot2, lSystem;
-let ellipseSize = 10;
-let greenValue = 0;
+let flock;
 
 function setup() {
-  //rectMode(CENTER);
+  createCanvas(1080, 720);
+  createP('Drag the mouse to generate new boids.');
 
-  can = createCanvas(1920, 1080);
-  background(0);
-  stroke(0, 0, 0, 255);
-  frameRate(45);
-  circdot1 = new Circdot(width / 2, height / 2, 50, 90, 1);
-  circdot2 = new Circdot(width / 2, height / 2, 50, 90, -1);
-  const rules = [
-    ['A', '-BF+AFA+FB-'],
-    ['B', '+AF-BFB-FA+'],
-  ];
-  lSystem = new LSystem(rules, 'A');
+  flock = new Flock();
 
-  for (let i = 0; i < 5; i++) {
-    lSystem.generate();
+  // Add an initial set of boids into the system
+  for (let i = 0; i < 100; i++) {
+    let b = new Boid(width / 2, height / 2);
+    flock.addBoid(b);
   }
-  //noLoop();
-}
 
-let rectWidth = 1000;
-let rectHeight = 800;
+  describe(
+    'A group of bird-like objects, represented by triangles, moving across the canvas, modeling flocking behavior.'
+  );
+}
 
 function draw() {
-  if (frameCount == 1 || frameCount == 351 || frameCount == 701) {
-    background(50);
-  }
-
-  if (frameCount <= 350) {
-    background(50, 10);
-    greenValue += 10;
-  } else if (frameCount <= 700) {
-    let growthFactor = map(frameCount, 301, 600, 0, width * 0.5); 
-    fill(50, 10);
-    rect((width - growthFactor) / 2, (height - growthFactor) / 2, growthFactor, growthFactor); 
-    changeRules();
-  } else {
-    circdot1.step = 100;  
-    circdot1.angle = 35;
-    circdot2.step = 50;
-    circdot2.angle = 35;
-  }
-
-  drawIt(circdot1, lSystem.thestring[circdot1.whereinstring]);
-  drawIt(circdot2, lSystem.thestring[circdot2.whereinstring]);
-
-  circdot1.whereinstring++;
-  circdot2.whereinstring++;
-  if (circdot1.whereinstring > lSystem.thestring.length - 1) circdot1.whereinstring = 0;
-  if (circdot2.whereinstring > lSystem.thestring.length - 1) circdot2.whereinstring = 0;
-
-  //recordit();
-  console.log(frameCount);
+  background(0);
+  flock.run();
 }
 
+// On mouse drag, add a new boid to the flock
+function mouseDragged() {
+  flock.addBoid(new Boid(mouseX, mouseY));
+}
 
-
-
-class Circdot {
-  constructor(x, y, step, angle, direction = 1) {
-    this.x = x;
-    this.y = y;
-    this.currentangle = 0;
-    this.step = step;
-    this.angle = angle;
-    this.direction = direction;
-    this.whereinstring = 0;
+// Flock class to manage the array of all the boids
+class Flock {
+  constructor() {
+    // Initialize the array of boids
+    this.boids = [];
   }
 
-  moveForward() {
-    let startX = this.x;
-    let startY = this.y;
-
-    let x1 = this.x + this.step * cos(radians(this.currentangle));
-    let y1 = this.y + this.step * sin(radians(this.currentangle));
-
-    if (x1 < 0) {
-      x1 = width;
-      ellipseSize = 10;
-    } else if (x1 > width) {
-      x1 = 0;
-      ellipseSize = 10;
+  run() {
+    for (let boid of this.boids) {
+      // Pass the entire list of boids to each boid individually
+      boid.run(this.boids);
     }
-
-    if (y1 < 0) {
-      y1 = height;
-      ellipseSize = 10;
-    } else if (y1 > height) {
-      y1 = 0;
-      ellipseSize = 10;
-    }
-
-    line(startX, startY, x1, y1);
-    this.x = x1;
-    this.y = y1;
   }
 
-  turnLeft() {
-    this.currentangle += this.angle * this.direction;
-  }
-
-  turnRight() {
-    this.currentangle -= this.angle * this.direction;
+  addBoid(b) {
+    this.boids.push(b);
   }
 }
 
-class LSystem {
-  constructor(rules, axiom) {
-    this.rules = rules;
-    this.thestring = axiom;
+class Boid {
+  constructor(x, y) {
+    this.acceleration = createVector(0, 0);
+    this.velocity = createVector(random(-1, 1), random(-1, 1));
+    this.position = createVector(x, y);
+    this.size = 3.0;
+
+    // Maximum speed
+    this.maxSpeed = 3;
+
+    // Maximum steering force
+    this.maxForce = 0.05;
+    colorMode(HSB);
+    this.color = color(random(256), 255, 255);
   }
 
-  generate() {
-    let outputstring = '';
-
-    for (let i = 0; i < this.thestring.length; i++) {
-      let ismatch = false;
-      for (let rule of this.rules) {
-        if (this.thestring[i] == rule[0]) {
-          outputstring += rule[1];
-          ismatch = true;
-          break;
-        }
-      }
-      if (!ismatch) outputstring += this.thestring[i];
-    }
-
-    this.thestring = outputstring;
-    return this.thestring;
+  run(boids) {
+    this.flock(boids);
+    this.update();
+    this.borders();
+    this.render();
   }
-}
 
-function changeRules() {
-  lSystem.rules[0] = ['A', '+BF-AFA-FB+'];
-  lSystem.rules[1] = ['B', '-AF+FBF+FA-'];
-  circdot1.angle = 60;
-  circdot2.angle = 60;
-  circdot1.step = 50;
-  circdot2.step = 50;
-}
-
-function drawIt(circdot, k) {
-  if (k == 'F') {
-    let startX = circdot.x;
-    let startY = circdot.y;
-    circdot.moveForward();
-    let endX = circdot.x;
-    let endY = circdot.y;
-    line(startX, startY, endX, endY);
-    mirrorShapes(circdot);
-
-    push();
-    scale(-1, 1);
-    translate(-width, 0);
-    line(startX, startY, endX, endY);
-    mirrorShapes(circdot);
-    pop();
-  } else if (k == '+') {
-    circdot.turnLeft();
-  } else if (k == '-') {
-    circdot.turnRight();
+  applyForce(force) {
+    // We could add mass here if we want: A = F / M
+    this.acceleration.add(force);
   }
-}
 
-function EsDots(x, y, size, color) {
-  fill(color);
-  noStroke();
-  ellipse(x, y, size, size);
-}
+  // We accumulate a new acceleration each time based on three rules
+  flock(boids) {
+    let separation = this.separate(boids);
+    let alignment = this.align(boids);
+    let cohesion = this.cohesion(boids);
 
-function mirrorShapes(circdot) {
-  if (frameCount > 700) {
-    push();
-    translate(circdot.x / 40, circdot.y / 40);
-    EsDots(circdot.x, circdot.y, ellipseSize, color(28, 255, 5, 200));
-    EsDots(circdot.x + 20, circdot.y + 20, ellipseSize * 0.8, color(0, 0, 255, 200));
-    EsDots(circdot.x - 20, circdot.y - 20, ellipseSize * 0.8, color(0, 255, 255, 200));
-    EsDots(circdot.x - 20, circdot.y - 40, ellipseSize * 0.7, color(255, 20, 55, 200));
-    EsDots(-circdot.x - 80, -circdot.y - 60, ellipseSize * 0.9, color(255, 20, 55, 200));
-    EsDots(circdot.x - 80, circdot.y - 80, ellipseSize * 0.5, color(255, 255, 0));
+    // Arbitrarily weight these forces
+    separation.mult(1.5);
+    alignment.mult(1.0);
+    cohesion.mult(1.0);
 
-    ellipseSize += 0.9;
-    pop();
+    // Add the force vectors to acceleration
+    this.applyForce(separation);
+    this.applyForce(alignment);
+    this.applyForce(cohesion);
+  }
 
-  } else if (frameCount > 350) {
-    fill(135, greenValue, 235, 200);
-    ellipse(circdot.x, circdot.y, 50, 50, 10);
+  // Method to update location
+  update() {
+    // Update velocity
+    this.velocity.add(this.acceleration);
+
+    // Limit speed
+    this.velocity.limit(this.maxSpeed);
+    this.position.add(this.velocity);
+
+    // Reset acceleration to 0 each cycle
+    this.acceleration.mult(0);
+  }
+
+  // A method that calculates and applies a steering force towards a target
+  // STEER = DESIRED MINUS VELOCITY
+  seek(target) {
+    // A vector pointing from the location to the target
+    let desired = p5.Vector.sub(target, this.position);
+
+    // Normalize desired and scale to maximum speed
+    desired.normalize();
+    desired.mult(this.maxSpeed);
+
+    // Steering = Desired minus Velocity
+    let steer = p5.Vector.sub(desired, this.velocity);
+
+    // Limit to maximum steering force
+    steer.limit(this.maxForce);
+    return steer;
+  }
+
+  render() {
+    // Draw a triangle rotated in the direction of velocity
+    let theta = this.velocity.heading() + radians(90);
+    fill(this.color);
     stroke(255);
-    strokeWeight(2);
-    line(circdot.x, circdot.y, width / 2, height / 2);
-
-  } else {
-    let radius = random(10, 50);
-    EsDots(circdot.x, circdot.y, radius, color(64, greenValue, 208));
-    EsDots(circdot.x - 2, circdot.y - 2, radius - 2, color(244, 0, 0));
-    EsDots(circdot.x - 4, circdot.y - 6, radius - 24, color(0, 244, 0));
-    EsDots(circdot.x - 6, circdot.y - 8, radius - 16, color(100, 150, 0));
+    push();
+    translate(this.position.x, this.position.y);
+    rotate(theta);
+    beginShape();
+    vertex(0, -this.size * 2);
+    vertex(-this.size, this.size * 2);
+    vertex(this.size, this.size * 2);
+    endShape(CLOSE);
+    pop();
   }
-}
 
-function keyPressed() {
-  if (keyIsPressed === true) {
-    let k = key;
-    console.log("k is " + k);
-
-    if (k == 's' || k == 'S') {
-      console.log("Stopped Recording");
-      recMode = false;
-      noLoop();
+  // Wraparound
+  borders() {
+    if (this.position.x < -this.size) {
+      this.position.x = width + this.size;
     }
 
-    if (k == ' ') {
-      console.log("Start Recording");
-      recMode = true;
-      loop();
+    if (this.position.y < -this.size) {
+      this.position.y = height + this.size;
+    }
+
+    if (this.position.x > width + this.size) {
+      this.position.x = -this.size;
+    }
+
+    if (this.position.y > height + this.size) {
+      this.position.y = -this.size;
     }
   }
-}
 
-function recordit() {
-  if (recMode == true) {
-    let ext = nf(frameCount, 4);
-    saveCanvas(can, 'anim/frame-' + ext, 'jpg');
-    console.log("rec " + ext);
+  // Separation
+  // Method checks for nearby boids and steers away
+  separate(boids) {
+    let desiredSeparation = 25.0;
+    let steer = createVector(0, 0);
+    let count = 0;
+
+    // For every boid in the system, check if it's too close
+    for (let boid of boids) {
+      let distanceToNeighbor = p5.Vector.dist(this.position, boid.position);
+
+      // If the distance is greater than 0 and less than an arbitrary amount (0 when you are yourself)
+      if (distanceToNeighbor > 0 && distanceToNeighbor < desiredSeparation) {
+        // Calculate vector pointing away from neighbor
+        let diff = p5.Vector.sub(this.position, boid.position);
+        diff.normalize();
+
+        // Scale by distance
+        diff.div(distanceToNeighbor);
+        steer.add(diff);
+
+        // Keep track of how many
+        count++;
+      }
+    }
+
+    // Average -- divide by how many
+    if (count > 0) {
+      steer.div(count);
+    }
+
+    // As long as the vector is greater than 0
+    if (steer.mag() > 0) {
+      // Implement Reynolds: Steering = Desired - Velocity
+      steer.normalize();
+      steer.mult(this.maxSpeed);
+      steer.sub(this.velocity);
+      steer.limit(this.maxForce);
+    }
+    return steer;
   }
-}
+
+  // Alignment
+  // For every nearby boid in the system, calculate the average velocity
+  align(boids) {
+    let neighborDistance = 50;
+    let sum = createVector(0, 0);
+    let count = 0;
+    for (let i = 0; i < boids.length; i++) {
+      let d = p5.Vector.dist(this.position, boids[i].position);
+      if (d > 0 && d < neighborDistance) {
+        sum.add(boids[i].velocity);
+        count++;
+      }
+    }
+    if (count > 0) {
+      sum.div(count);
+      sum.normalize();
+      sum.mult(this.maxSpeed);
+      let steer = p5.Vector.sub(sum, this.velocity);
+      steer.limit(this.maxForce);
+      return steer;
+    } else {
+      return createVector(0, 0);
+    }
+  }
+
+  // Cohesion
+  // For the average location (i.e., center) of all nearby boids, calculate steering vector towards that location
+  cohesion(boids) {
+    let neighborDistance = 50;
+    let sum = createVector(0, 0); // Start with empty vector to accumulate all locations
+    let count = 0;
+    for (let i = 0; i < boids.length; i++) {
+      let d = p5.Vector.dist(this.position, boids[i].position);
+      if (d > 0 && d < neighborDistance) {
+        sum.add(boids[i].position); // Add location
+        count++;
+      }
+    }
+    if (count > 0) {
+      sum.div(count);
+      return this.seek(sum); // Steer towards the location
+    } else {
+      return createVector(0, 0);
+    }
+  }
+} // class Boid
